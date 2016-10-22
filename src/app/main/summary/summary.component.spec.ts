31cd033f726e759337f6d1eb734c60308fdbc67f
mkishine <mkishine@yahoo.com>
2016-10-21 18:47:44 -0700

    first batch of tests for summary component

--- src/app/main/summary/summary.component.spec.ts
+++ src/app/main/summary/summary.component.spec.ts
@@ -1,11 +1,67 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { SummaryComponent } from './summary.component.ts';
 
 describe('Component: Summary', () => {
+  let fixture:ComponentFixture<SummaryComponent>;
+  let comp:SummaryComponent;
+  const title = 'User';
+  const type = 'Time';
+  const data:[string, number][] = [['a', 1], ['b', 2], ['c', 3]];
+
+  // async beforeEach
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
+      declarations: [SummaryComponent],
+    })
+      .compileComponents(); // compile template and css
+  }));
+
+  // synchronous beforeEach
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SummaryComponent);
+    comp = fixture.componentInstance;
+
+    comp.title = title;
+    comp.type = type;
+    comp.data = data;
+    fixture.detectChanges(); // trigger initial data binding
+  });
+
   it('should create an instance', () => {
-    let component = new SummaryComponent();
-    expect(component).toBeTruthy();
+    expect(comp).toBeTruthy();
+  });
+
+  it('should display title', () => {
+    const titleEl = fixture.debugElement.query(By.css('.summary-title'));
+    expect(titleEl.nativeElement.textContent).toBe(title);
+  });
+
+  it('should display type', () => {
+    const typeEl = fixture.debugElement.query(By.css('.summary-type'));
+    expect(typeEl.nativeElement.textContent).toBe(type);
+  });
+
+  it('should display summary data', () => {
+    let xEls = fixture.debugElement.queryAll(By.css('.summary-x'));
+    expect(xEls.length).toBe(data.length, 'check data length');
+    expect(xEls[0].nativeElement.textContent).toBe(data[0][0]);
+  });
+
+  it('should allow data selection', () => {
+    const label = data[1][0];
+    const id = `#${comp.idBase}${label}`;
+    const expectedEvent = [title, label];
+    let observedEvent:[string, string];
+    let rbEl = fixture.debugElement.query(By.css(id));
+    comp.onSummarySelected.subscribe((event:[string, string]) => observedEvent = event);
+    rbEl.triggerEventHandler('click', null);
+    expect(expectedEvent).toEqual(observedEvent);
   });
 });
--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -19,22 +19,19 @@ export class SummaryComponent implements OnInit, OnChanges {
   @Input() data:[string, number][];
   @Input() selectionTitle:string;
   @Input() selectionValue:string;
-  @Output() onSummarySelected = new EventEmitter<[string,string]>();
+  @Output() onSummarySelected = new EventEmitter<[string, string]>();
 
-  private id:string;
+  public idBase:string;
+
   private control:FormControl;
   private formGroup:FormGroup;
 
   ngOnInit() {
-    this.id = this.title + this.type;
+    this.idBase = `summary_${this.title}_${this.type}_`;
     this.control = new FormControl();
     let controls = {};
-    controls[this.id] = this.control;
+    controls[this.idBase] = this.control;
     this.formGroup = new FormGroup(controls);
-  }
-
-  private onClick(newValue:string) {
-    this.onSummarySelected.emit([this.title, newValue]);
   }
 
   ngOnChanges(changes:SimpleChanges) {
@@ -45,10 +42,15 @@ export class SummaryComponent implements OnInit, OnChanges {
       return;
     }
     const newTitle = changes['selectionTitle'] ? changes['selectionTitle'].currentValue : this.title;
-    const newValue = newTitle == this.title ? changes['selectionValue'].currentValue : '';
-    if ( newValue == this.control.value ) {
+    const newValue = newTitle === this.title ? changes['selectionValue'].currentValue : '';
+    if (newValue === this.control.value) {
       return;
     }
     this.control.reset(newValue);
+  }
+
+  /* tslint:disable:no-unused-variable */
+  private onClick(newValue:string) {
+    this.onSummarySelected.emit([this.title, newValue]);
   }
 }
